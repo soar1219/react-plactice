462fec5d62ae63d29934c11a8c45e126dd7b0271
soar1219 <tsubasa1219.info@gmail.com>
2023-09-08 02:37:09 +0900

    分割代入

--- react_plactice/src/App.js
+++ react_plactice/src/App.js
@@ -73,3 +73,27 @@ console.log(val5); */
 // const func3 = (num1, num2) =>num1 + num2;
 
 // console.log(func3(1,2));
+
+/* 分割代入 */
+// const myProfile = {
+//   name: "つばさ",
+//   age:28,
+// };
+
+// const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}歳です。`;
+
+// console.log(message1);
+
+// const {name, age} = myProfile;
+// const message2 = `名前は${name}です。年齢は${age}歳です。`;
+
+// console.log(message2);
+
+// const myProfile = ["つばさ", 28];
+// const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です。`;
+// console.log(message3);
+
+// const [name, age] = myProfile;
+// const message4 = `名前は${name}です。年齢は${age}歳です。`
+
+// console.log(message4);
