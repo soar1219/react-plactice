22101bfe67c4942a820c5cd3c0e095c33bec8e2d
soar1219 <tsubasa1219.info@gmail.com>
2023-09-07 23:41:13 +0900

    テンプレート文字列

--- react_plactice/src/App.js
+++ react_plactice/src/App.js
@@ -42,3 +42,14 @@ val5[0] = "bird";
 val5.push("monky");
 console.log(val5); */
 
+// /* テンプレート文字列 */
+// const name = "つばさ";
+// const age = 28;
+// /* 私の名前はつばさです。年齢は28歳です。 */
+// /* 従来の方法 */
+// const message1 = "私の名前は" + name +"です。年齢は" + age + "歳です。";
+// console.log(message1);
+
+// /* テンプレート文字列を用いた方法 */
+// const message2 = `私の名前は${name}です。年齢は${age}歳です。`;
+// console.log(message2);
